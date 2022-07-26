6149e3a9fe1f9ada222a5c9f19152a1ffbaa92f1
Szymon271000 <80632543+Szymon271000@users.noreply.github.com>
2022-07-26 14:38:07 +0200

    Added UpdateArtist, Album and Gender in SongController

--- WebApplication1/WebApplication1/Controllers/SongController.cs
+++ WebApplication1/WebApplication1/Controllers/SongController.cs
@@ -13,11 +13,19 @@ namespace WebApplication1.Controllers
     public class SongController : ControllerBase
     {
         private readonly IBaseRepository<Song> _songRepository;
+        private readonly IBaseRepository<Artist> _artistRepository;
+        private readonly IBaseRepository<Album> _albumRepository;
+        private readonly IBaseRepository<Genre> _genreRepository;
+
+
 
         private readonly IMapper _mapper;
-        public SongController(IBaseRepository<Song> songRepository, IMapper mapper)
+        public SongController(IBaseRepository<Song> songRepository, IBaseRepository<Artist> artistRepository, IBaseRepository<Album> albumRepository, IBaseRepository<Genre> genreRepository, IMapper mapper)
         {
             _songRepository = songRepository;
+            _genreRepository = genreRepository;
+            _albumRepository = albumRepository;
+            _artistRepository = artistRepository;
             _mapper = mapper;
         }
 
@@ -76,6 +84,60 @@ namespace WebApplication1.Controllers
 
             await _songRepository.Delete(songToDelete);
             return NoContent();
+        }
+
+        [HttpPut("{id}/artist/{artistId}")]
+        public async Task<IActionResult> UpdateArtistOfSong(int id, int artistId)
+        {
+            var song = await _songRepository.GetById(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            var artist = await _artistRepository.GetById(artistId);
+            if (artist == null)
+            {
+                return NotFound();
+            }
+            song.ArtistId = artist.Id;
+            await _songRepository.Update(song);
+            return Ok();
+        }
+
+        [HttpPut("{id}/album/{albumId}")]
+        public async Task<IActionResult> UpdateAlbumtOfSong(int id, int albumId)
+        {
+            var song = await _songRepository.GetById(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            var album = await _albumRepository.GetById(albumId);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            song.AlbumId = album.Id;
+            await _songRepository.Update(song);
+            return Ok();
+        }
+
+        [HttpPut("{id}/genre/{genreId}")]
+        public async Task<IActionResult> UpdateGenreOfSong(int id, int genreId)
+        {
+            var song = await _songRepository.GetById(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            var genre = await _genreRepository.GetById(genreId);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+            song.GenreId = genre.Id;
+            await _songRepository.Update(song);
+            return Ok();
         }
     }
 }
